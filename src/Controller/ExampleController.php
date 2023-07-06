8f7aefb09d47653493de4c73cf8d86d5358291f7
matieg <matiasgutierrez.dev@gmail.com>
2023-07-06 19:17:05 -0300

    post request controller

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -3,15 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\Type\UserFormType;
 use App\Repository\UserRepository;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
+// use Symfony\Component\Serializer\SerializerInterface;
 // use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
-use Symfony\Component\Serializer\Annotation\Context;
+// use Symfony\Component\Serializer\Annotation\Context;
 
 class ExampleController extends AbstractController
 {
@@ -22,22 +24,26 @@ class ExampleController extends AbstractController
     {
         $users = $userRepository->findAll();
 
-        return $this->json([$users], context: [ 'groups' => ['user'] ]);
+        return $this->json($users, 200);
+        // return $this->json($users, context: [ 'groups' => ['user'] ]);
     }
 
-    #[Route('/create', name: 'app_create')]
-    public function create(Request $request, UserRepository $userRepository): JsonResponse
+    #[Route( path:'/create', name: 'app_create', methods: ['POST'] )]
+    public function create(Request $request, UserRepository $userRepository)
     {
-        try {
+
+        try{
+            // json_decode($request->getContent(), true);
             $user = new User();
-            $user->setName($request->get('name'));
-            $user->setUsername($request->get('username'));
-            $user->setPassword($request->get('password'));
-            
-            $userRepository->save($user, true);
-            return new JsonResponse('asdasdasd');
-        } catch(Exception $e) {
-            return new JsonResponse($e->getMessage());
+            $form = $this->createForm( UserFormType::class, $user );
+            $form->handleRequest($request);
+            if( $form->isSubmitted() && $form->isValid() ){
+                $userRepository->save($user, true);
+                return $this->json( ['message'=>'mensaje enviado'], Response::HTTP_OK );
+            }
+        } catch (Exception $e ){
+
+            return $this->json( ['message'=> $e->getMessage()], Response::HTTP_OK );
         }
     }
 }
